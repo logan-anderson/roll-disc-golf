fcece69c1a8df181e1bae0e899e07eb05289d392
Logan Anderson <landerson2@upei.ca>
2022-01-09 13:00:02 -0500

    added files

---
+++ src/components/state/options.ts
@@ -0,0 +1,68 @@
+import { Option } from "../../util/interfaces";
+
+const throwOptions: Option[] = [
+  {
+    label: "Wild",
+    prob: 0.12,
+  },
+  {
+    label: "Backhand",
+    prob: 0.11,
+  },
+  {
+    label: "Forehand",
+    prob: 0.11,
+  },
+  {
+    label: "Roller",
+    prob: 0.11,
+  },
+  {
+    label: "Overhand",
+    prob: 0.11,
+  },
+  {
+    label: "Wild",
+    prob: 0.11,
+  },
+  {
+    label: "Over Stable",
+    prob: 0.11,
+  },
+  {
+    label: "Under Stable",
+    prob: 0.11,
+  },
+  {
+    label: "Neutral",
+    prob: 0.11,
+  },
+];
+
+const discOptions: Option[] = [
+  {
+    label: "Putter",
+    prob: 0.2,
+  },
+  {
+    label: "Midrange",
+    prob: 0.2,
+  },
+  {
+    label: "Fairway Diver",
+    prob: 0.2,
+  },
+  {
+    label: "Distance Diver",
+    prob: 0.2,
+  },
+  {
+    label: "Wild",
+    prob: 0.2,
+  },
+];
+
+export const DefaultOptionTypeValues = [
+  { options: throwOptions, label: "Throw Type" },
+  { options: discOptions, label: "Disc Type" },
+];
